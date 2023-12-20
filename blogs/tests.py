0a15df3553d6308d8f7a3c45a5dc68d2dc92bbfd
Scott Stevson <scott@stevson.com>
2023-12-20 15:50:07 -0800

    adding first test case

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class PostTestCase(TestCase):
+    fixtures = ['blogs_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
