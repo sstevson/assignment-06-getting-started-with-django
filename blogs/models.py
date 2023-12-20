9a851824b242791fcc990b75e6b2cadf45816c40
Scott Stevson <scott@stevson.com>
2023-12-20 12:20:08 -0800

    adding the initial Post model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True, null=True)
