3ff24289a106d19fbd5207c0b5f7aca8539e23bf
Scott Stevson <scott@stevson.com>
2023-12-21 06:00:54 -0800

    adding the list view function

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogs.models import Post
 
 
 def stub_view(request, *args, **kwargs):
@@ -10,3 +12,12 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
