c5c3acd52f492265ad1859cb6c971947d12fe336
Scott Stevson <scott@stevson.com>
2023-12-21 06:48:29 -0800

    adding the detail function

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
 from blogs.models import Post
@@ -20,3 +20,13 @@ def list_view(request):
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'blogs/list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogs/detail.html', context)
