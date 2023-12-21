d5a683d6ce92bbd9b287a76de4f12f5e706d410a
Scott Stevson <scott@stevson.com>
2023-12-21 10:51:58 -0800

    adding stubs for new classes

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
 from blogs.models import Post, Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Post)
 admin.site.register(Category)
