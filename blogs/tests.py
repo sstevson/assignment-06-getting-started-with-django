bfc62f7a4263a959d5ed8aef9489dfb252ecd552
Scott Stevson <scott@stevson.com>
2023-12-21 05:58:26 -0800

    adding tests for the list views

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,7 @@
+# Using datetime timezone.utc below because the django
+# import appears to no longer be valid
+from datetime import datetime, timedelta, timezone
+
 from django.test import TestCase
 from django.contrib.auth.models import User
 
@@ -28,3 +32,34 @@ class CategoryTestCase(TestCase):
         c1 = Category(name = expected)
         actual = str(c1)
         self.assertEqual(expected, actual)
+
+
+class FrontEndTestCase(TestCase):
+    """test views provided in the front-end"""
+    fixtures = ['blogs_test_fixture.json', ]
+
+    def setUp(self):
+        self.now = datetime.utcnow().replace(tzinfo=timezone.utc)
+        self.timedelta = timedelta(15)
+        author = User.objects.get(pk=1)
+        for count in range(1, 11):
+            post = Post(title="Post %d Title" % count,
+                        text="foo",
+                        author=author)
+            if count < 6:
+                # publish the first five posts
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+        # the content of the rendered response is always a bytestring
+        resp_text = resp.content.decode(resp.charset)
+        self.assertTrue("Recent Posts" in resp_text)
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            if count < 6:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
