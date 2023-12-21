a77b1d13789b45bda2e7a1df9eb155176015c77a
Scott Stevson <scott@stevson.com>
2023-12-21 09:45:38 -0800

    adding a default for an empty string after the base url

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(template_name='login.html'), name='login'),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
