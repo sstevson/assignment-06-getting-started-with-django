83873c85eccf108768739e199c7987fdaeaf6012
Scott Stevson <scott@stevson.com>
2023-12-21 06:01:19 -0800

    including the blog urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('', admin.site.urls)
+    path('', include('blogs.urls'))
 ]
